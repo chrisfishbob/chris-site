b6bd9ccfd4baec44c6fdda1804b6eb52cf80c5f4
Chris Hsu <chrisfishbob@gmail.com>
2022-07-10 16:19:47 -0700

    more refactoring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import CategoryItem from "./components/category-item/category-item.component";
+import Directory from "./components/directory/directory.component";
 
 function App() {
   const categories = [
@@ -31,11 +31,8 @@ function App() {
   ];
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <Directory categories={categories} />
+    
   );
 }
 
