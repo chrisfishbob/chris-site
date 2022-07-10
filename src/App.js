a6694582ab2cd59256c21990c4cbdf9270ccf067
Chris Hsu <chrisfishbob@gmail.com>
2022-07-10 16:02:20 -0700

    Refactored category item and introduced images:

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
 import "./App.css";
-import "./categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component";
 
 function App() {
   const categories = [
     {
       id: 1,
       title: "Hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
     },
     {
       id: 2,
       title: "Jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
     },
     {
       id: 3,
       title: "Sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
     },
     {
       id: 4,
       title: "Womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
     },
     {
       id: 5,
       title: "Mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
 
   return (
     <div className="categories-container">
-      {/* // The return is implicit here */}
-      {categories.map(({title}) => (
-        <div className="category-container">
-          <div className="backgroud-image" />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
       ))}
     </div>
   );
