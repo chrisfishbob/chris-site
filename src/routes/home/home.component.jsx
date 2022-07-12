bcfa35fafa4e09957ab280559c846a5cc83de86f
Chris <chrisfishbob@gmail.com>
2022-07-12 16:46:13 -0700

    New images

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -7,39 +7,40 @@ function Home() {
       id: 1,
       title: "My Story",
       subtitle: "See More",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      imageUrl: "https://images.unsplash.com/photo-1598935898639-81586f7d2129?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dGFpcGVpfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
     },
     {
       id: 2,
       title: "Work Experience",
       subtitle: "See More",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      imageUrl: "https://www.officelovin.com/wp-content/uploads/2016/10/deloitte-toronto-office-2.jpg",
     },
     {
       id: 3,
       title: "Education",
       subtitle: "See More",
-      imageUrl: "https://github.com/chrisfishbob/webstore/blob/main/poly.jpg?raw=true",
+      imageUrl:
+        "https://content-calpoly-edu.s3.amazonaws.com/scholars/1/rotator_images/calpoly-aerial_0.jpg",
     },
     {
       id: 4,
       title: "Technical Skills",
       subtitle: "See More",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      imageUrl: "https://3s81si1s5ygj3mzby34dq6qf-wpengine.netdna-ssl.com/wp-content/uploads/2019/01/ab_datacenter_generic_8888-1030x438.jpg"
     },
     {
       id: 5,
       title: "Projects",
       subtitle: "See More",
       imageUrl:
-        "https://github.com/chrisfishbob/webstore/blob/main/Chess.jpeg?raw=true",
+        "https://images.unsplash.com/photo-1580541832626-2a7131ee809f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2hlc3N8ZW58MHx8MHx8&w=1000&q=80",
     },
   ];
 
   return (
     <div>
       {/* Location of outlet matters! */}
-      <Outlet/>
+      <Outlet />
       <Directory categories={categories} />
     </div>
   );
