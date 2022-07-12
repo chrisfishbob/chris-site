2f28a2a306e16b59e5fbc723633daa5ff92be3ae
Chris <chrisfishbob@gmail.com>
2022-07-12 15:33:55 -0700

    Menu bar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import "./App.css";
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
+
+function Shop() {
+  return <h1>Sample website!</h1>;
+}
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "Hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
   return (
-    <Directory categories={categories} />
-    
+    <Routes>
+      {/* Render the element if the path matches*/}
+      <Route path="/" element={<Navigation/>}>
+          <Route index element={<Home/>}/>
+          <Route path="shop" element={<Shop></Shop>} />
+      </Route>
+      <Route path="/shop" element={<Shop />} />
+    </Routes>
   );
 }
 
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+import logo from "../../assets/greyhd.jpeg"
+import "./navigation.styles.scss"
+
+function Navigation() {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+            <img src={logo} width={50}/>
+        </Link>
+
+        <div className="links-container">
+          <Link className="nav-links-container" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+}
+
+export default Navigation;
