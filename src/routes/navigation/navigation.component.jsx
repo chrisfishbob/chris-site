dbd0892153cc9968755c23baea7c3dcf10ce2d98
Chris <chrisfishbob@gmail.com>
2022-07-20 19:53:38 -0700

    Info Link added

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,22 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
-import logo from "../../assets/paw2.png"
-import "./navigation.styles.scss"
+import logo from "../../assets/paw2.png";
+import "./navigation.styles.scss";
 
 function Navigation() {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-            <img src={logo} width={50}/>
+          <img src={logo} width={50} />
         </Link>
 
-        <div className="links-container">
-          <Link className="nav-links-container" to="/contact">
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/contact">
             CONTACT
+          </Link>
+          <Link className="nav-link" to="/info">
+            INFO
           </Link>
         </div>
       </div>
