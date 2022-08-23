5bf8ea43b0e171ccb9aef7d811cc17a77cbde4fc
Chris Hsu <chrisfishbob@gmail.com>
2022-08-22 19:12:55 -0700

    Adding more_blogs branch to git repo

--- src/routes/my-story/my-story.component.jsx
+++ src/routes/my-story/my-story.component.jsx
@@ -24,6 +24,12 @@ function MyStory() {
             <img className="poly-img" src={deloitte_boat} />
             <p> I had the great oppurtunity to work as a Software Engineer
             this summer at Deloitte!</p>
+            <p> This website exists solely because of my time working at the firm. To say that
+            I was "concerned" would be an understatement when I first got my project details and
+            learnt that I had to use HTML, CSS, JavaScript, AWS, and ReactJS, which is awesome, except ...</p>
+            <p> I didn't know any of it</p>
+            <p> But after many hours, 5000+ lines, and many pull requests, I've learned enough
+            to build this website with everything mentioned above and deploy it!</p>
           </div>
 
           <div className="event">
