8ac166d90585ec84f4cdb89e80a91e399ec62b08
Chris Hsu <chrisfishbob@gmail.com>
2022-08-22 17:05:12 -0700

    Blog Implementation Started

--- src/routes/home/my-story/my-story.component.jsx
+++ src/routes/home/my-story/my-story.component.jsx
@@ -1,23 +1,35 @@
-import "./my-story.styles.scss"
-
+import "./my-story.styles.scss";
 
 function MyStory() {
   const imageUrl =
     "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/1200px-President_Barack_Obama.jpg";
+  
+  const poly = "https://gradbusiness.calpoly.edu/wp-content/uploads/2020/11/20200113-CPAerials-SP0062.jpg"
+
   return (
-    <div className="my-story-container">
-      <img src={imageUrl} className="profile-pic" />
-      <div className="profile-text">
-        <h1>Hello There!</h1>
-        <h2>I'm Chris, nice to meet you!</h2>
-        <h2>I am Chris</h2>
-        <h2>I am Chris</h2>
-        <h2>I am Chris</h2>
-        <h2>I am Chris</h2>
-        <h2>I am Chris</h2>
+    <div>
+      <div className="row">
+        <div className="left-column"></div>
+        <div className="event">
+          <h1> School </h1>
+          <h3> Cal Poly San Luis Obispo</h3>
+
+        </div>
+        <div className="event">
+          <h1> Birth </h1>
+        </div>
+        <div className="right-column">
+          <div className="event">
+            <h1> About Me </h1>
+          </div>
+          <div className="event">
+            <h1> Links </h1>
+          </div>
+        </div>
       </div>
+      <div className="footer"></div>
     </div>
   );
 }
 
-export default MyStory
+export default MyStory;
