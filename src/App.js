1fac98d60a0d9684387b3695a5fa8de910017e55
Chris Hsu <chrisfishbob@gmail.com>
2022-09-10 14:10:31 -0700

    Home name changed to blog

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Navigation from "./routes/navigation/navigation.component";
 import Projects from "./routes/projects/projects.component";
 import Pages from "./routes/pages/pages.component";
 import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component"
+import Blog from "./routes/blog/blog.component"
 import "./App.scss";
 
 function Construction() {
@@ -21,7 +21,7 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Pages/>} />
         <Route path="projects" element={<Projects/>} />
-        <Route path="blog" element={<Home/>} />
+        <Route path="blog" element={<Blog/>} />
         <Route path="construction" element={<Construction/>} />
 
       </Route>
