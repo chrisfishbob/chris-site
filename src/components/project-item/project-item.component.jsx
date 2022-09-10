6133ac7d65864b9a5e3634f5b189deac906ae513
Chris Hsu <chrisfishbob@gmail.com>
2022-09-10 13:11:42 -0700

    Link and anchor usage differentiated

--- src/components/project-item/project-item.component.jsx
+++ src/components/project-item/project-item.component.jsx
@@ -1,25 +1,54 @@
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import "./project-item.styles.scss";
 
 // Used for both projects and pages directory
 const ProjectItem = ({ project }) => {
   const { imageUrl, title, externalUrl, subtitle, openNewTab } = project;
-  return (
-    <Fragment>
-      <a className="project-container" href={externalUrl} target={openNewTab ? "_blank": ""}>
-        <div
-          className="background-image"
-          style={{
-            backgroundImage: `url(${imageUrl})`,
-          }}
-        />
-        <div className="project-body-container">
-          <h2>{title}</h2>
-          <p>{subtitle}</p>
-        </div>
-      </a>
-    </Fragment>
-  );
+  if (openNewTab) {
+    return (
+      <Fragment>
+        <a
+          className="project-container"
+          href={externalUrl}
+          target="_blank"
+        >
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
+          <div className="project-body-container">
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+          </div>
+        </a>
+      </Fragment>
+    );
+  }
+
+  else {
+    return (
+      <Fragment>
+        <Link
+          className="project-container"
+          to={externalUrl}
+        >
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
+          <div className="project-body-container">
+            <h2>{title}</h2>
+            <p>{subtitle}</p>
+          </div>
+        </Link>
+      </Fragment>
+    );
+  }
 };
 
 export default ProjectItem;
