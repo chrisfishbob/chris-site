e21e24e84fdf4e80e7ce7b8346f7ebc886569c61
Chris Hsu <chrisfishbob@gmail.com>
2022-09-16 16:18:36 -0700

    Education page complete

--- src/routes/education/education.component.jsx
+++ src/routes/education/education.component.jsx
@@ -1,8 +1,8 @@
 import "./education.styles.scss";
-import "./education-intro.styles.scss"
 import { useEffect } from "react";
 import "animate.css/animate.min.css";
 import { AnimationOnScroll } from "react-animation-on-scroll";
+import EducationIntro from "../../components/education-intro/education-intro.component";
 
 function Education() {
   useEffect(() => {
@@ -11,62 +11,63 @@ function Education() {
 
   return (
     <div>
-      <div className="education-intro-container">
-        <h1> My Education </h1>
-        <h3> Scroll Down to See My Course History</h3>
-      </div>
+      <EducationIntro />
 
       <div className="timeline">
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInLeft"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container left">
-          <div className="content-odd">
-            <h2>Fall 2022</h2>
-            <p>CSC-430: Programming Languages</p>
-            <p>CSC-466: Knowledge Discovery from Data</p>
-            <p>CSC-307: Software Engineering</p>
+          <div className="container left">
+            <div className="content-odd">
+              <h2>Fall 2022</h2>
+              <p>CSC-430: Programming Languages</p>
+              <p>CSC-466: Knowledge Discovery from Data</p>
+              <p>CSC-307: Software Engineering</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInRight"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container right">
-          <div className="content-even">
-            <h2>Spring 2022</h2>
-            <p>CSC-349 Design & Analysis of Algorithms</p>
-            <p>STAT-312 Stat Methods for Engineers</p>
-            <p>KINE-109 Bowling</p>
-            <p>BOT-121 General Botany</p>
+          <div className="container right">
+            <div className="content-even">
+              <h2>Spring 2022</h2>
+              <p>CSC-349 Design & Analysis of Algorithms</p>
+              <p>STAT-312 Stat Methods for Engineers</p>
+              <p>KINE-109 Bowling</p>
+              <p>BOT-121 General Botany</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInLeft"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container left">
-          <div className="content-odd">
-            <h2>Winter 2022</h2>
-            <p>CPE-315 Computer Architecture</p>
-            <p>PHYS-132 General Physics II</p>
-            <p>ANT-250 Biological Anthropology</p>
+          <div className="container left">
+            <div className="content-odd">
+              <h2>Winter 2022</h2>
+              <p>CPE-315 Computer Architecture</p>
+              <p>PHYS-132 General Physics II</p>
+              <p>ANT-250 Biological Anthropology</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInRight"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
           <div className="container right">
             <div className="content-even">
@@ -79,9 +80,10 @@ function Education() {
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInLeft"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
           <div className="container left">
             <div className="content-odd">
@@ -95,9 +97,10 @@ function Education() {
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInRight"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
           <div className="container right">
             <div className="content-even">
@@ -111,65 +114,69 @@ function Education() {
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInLeft"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container left">
-          <div className="content-odd">
-            <h2>Spring 2021</h2>
-            <p>CPE-101 Fundamentals of Computer Science</p>
-            <p>CPE-123 Introduction to Computing: Security</p>
-            <p>MATH-142 Calculus II</p>
-            <p>TH-210 Introduction to Theatre</p>
+          <div className="container left">
+            <div className="content-odd">
+              <h2>Spring 2021</h2>
+              <p>CPE-101 Fundamentals of Computer Science</p>
+              <p>CPE-123 Introduction to Computing: Security</p>
+              <p>MATH-142 Calculus II</p>
+              <p>TH-210 Introduction to Theatre</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInRight"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container right">
-          <div className="content-even">
-            <h2>Summer 2020 (Quarter Plus)</h2>
-            <p>COMS-101 Public Speaking</p>
-            <p>ES-102 Race, Culture, Poliics in the U.S.</p>
+          <div className="container right">
+            <div className="content-even">
+              <h2>Summer 2020 (Quarter Plus)</h2>
+              <p>COMS-101 Public Speaking</p>
+              <p>ES-102 Race, Culture, Poliics in the U.S.</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInLeft"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container left">
-          <div className="content-odd">
-            <h2>AP Credits 2020</h2>
-            <p>AP Calculus AB</p>
-            <p>AP Psychology</p>
-            <p>AP Microeconomics</p>
-            <p>AP English Literature and Composition</p>
-            <p>AP Art History</p>
+          <div className="container left">
+            <div className="content-odd">
+              <h2>AP Credits 2020</h2>
+              <p>AP Calculus AB</p>
+              <p>AP Psychology</p>
+              <p>AP Microeconomics</p>
+              <p>AP English Literature and Composition</p>
+              <p>AP Art History</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
 
         <AnimationOnScroll
-          animateIn="animate__fadeIn"
+          animateIn="animate__backInLeft"
           animateOut="animate__fadeOut"
-          duration={2}
+          offset={100}
+          duration={1}
         >
-        <div className="container right">
-          <div className="content-even">
-            <h2>AP Credits 2019</h2>
-            <p>AP Biology</p>
-            <p>AP English Language and Composition</p>
-            <p>AP U.S. History</p>
+          <div className="container right">
+            <div className="content-even">
+              <h2>AP Credits 2019</h2>
+              <p>AP Biology</p>
+              <p>AP English Language and Composition</p>
+              <p>AP U.S. History</p>
+            </div>
           </div>
-        </div>
         </AnimationOnScroll>
       </div>
     </div>
